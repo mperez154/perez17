8f7f2798d688645e38b8c34aa690ff5fabe24c74
mperez5 <mperez154@gmail.com>
2016-10-25 15:18:14 -0400

    Added Assignment 17_5

---
+++ src/application/Assignment_17_5.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Assignment_17_5 extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		int[] integers = {1,2,3,4,5};
+		Date date = new Date();
+		double myDouble = 5.5;
+		
+		FileInputStream input = new FileInputStream("C:\\Users\\mperez5\\Documents\\test.dat");
+		FileOutputStream output = new FileOutputStream("C:\\Users\\mperez5\\Documents\\test.dat");
+		
+
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+/*
+Write a program that stores an array of the five int values 1, 2, 3, 4, and 5, a Date object for the current time, and the double value 5.5 into the file named Exercise17_05.dat.
+*/
